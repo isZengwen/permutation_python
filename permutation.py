4fa3884498ed94f6b05c7502cbdf91d8a58dc491
isZengwen <42513777+isZengwen@users.noreply.github.com>
2018-09-02 13:51:31 +0800

    Initial commit

---
+++ permutation.py
@@ -0,0 +1,9 @@
+def pr(n):
+    count = 0
+    for i in range(10**(n-1),10**(n)):
+        length = len(set(str(i)))
+        if length == n and ('0' not in set(str(i))):
+            count += 1
+            print(i)
+    print("The number is :{}".format(count))
+pr(2)
